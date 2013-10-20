850b0566dc98d9b06bf6f166841e5959069b8cf0
zigzhang <davzz78@gmaio.com>
2013-10-19 18:02:03 -0700

    first

--- app/controllers/clinical_study_controller.rb
+++ app/controllers/clinical_study_controller.rb
@@ -1,2 +1,13 @@
 class ClinicalStudyController < ApplicationController
+  def index
+    @clinical_studies = ClinicalStudy.all
+    render json: {status: "success", data: @clinical_studies}
+  end
+  
+  def show
+    @clinical_study = ClinicalStudy.find params[:id]
+    @assessment = @clinical_study.assessment
+    render json: {status: "success", data: @assessment}
+  end
+  
 end
