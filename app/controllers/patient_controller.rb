035301f2b912f56e3c74ecd2e956b326d2f77616
Yann <yann.fleureau@gmail.com>
2013-10-19 19:35:52 -0700

    Connect to Android

--- app/controllers/patient_controller.rb
+++ app/controllers/patient_controller.rb
@@ -2,9 +2,9 @@ class PatientController < ApplicationController
   require 'rqrcode'
 
   def show
-    @patient = Patient.find params[:id]
-    @sponsor = @patient.site.sponsor
-    render json: {status: "success", data: @sponsor}
+    @patient = Patient.find(params[:id])
+    render json: {status: "success", data: {patient: @patient, protocole:  @patient.protocole, clinical_study: @patient.protocole.clinical_study,
+                                           assessments: @patient.protocole.assessments,  }}
   end
 
   def qrcode
